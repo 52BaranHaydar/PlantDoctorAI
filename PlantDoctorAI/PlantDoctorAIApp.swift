fb10dac381205cd816eaf642871ec97ad814b254
Baran Haydar <baranhaydar80@gmail.com>
2026-03-31 20:05:09 +0300

    Initial Commit

---
+++ PlantDoctorAI/PlantDoctorAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantDoctorAIApp.swift
+//  PlantDoctorAI
+//
+//  Created by Baran on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct PlantDoctorAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
